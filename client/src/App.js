38e2b88ecaa7e918b2486f04a0d1c93413b5a7c9
sam west <samwest0230@gmail.com>
2021-05-05 16:57:27 -0400

    added pin

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.scss';
 import { Route, Link, BrowserRouter, Switch } from 'react-router-dom'
+import pin from './assets/icons/pin.svg'
 import Nav from './components/nav'
 import Projects from './components/projects'
 import About from './components/about'
@@ -16,6 +17,8 @@ function App() {
             <div className='hero__card'>
               <h1 className='hero__card--name'>Sam West</h1>
               <h3 className='hero__card--title'>Full-Stack Developer</h3>
+              <img className='hero__card--pin' src={pin} />
+              <p className='hero__card--location'>Toronto</p>
             </div>
           </div>
           <div className='hero__right'>
